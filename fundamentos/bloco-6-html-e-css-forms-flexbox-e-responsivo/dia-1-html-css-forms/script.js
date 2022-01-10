619de43f708c54d59c1e45d3c0f8ab3a84bac328
Andre Santos <andrefelipe_santos@outlook.com>
2022-01-10 11:10:18 -0300

    exercicios inacabdos

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,8 +1,20 @@
-const estados = {'AC': 'Acre','AL': 'Alagoas','AP': 'Amapá','AM': 'Amazonas','BA': 'Bahia','CE': 'Ceará','ES': 'Espírito Santo','GO': 'Goiás','MA': 'Maranhão','MT': 'Mato Grosso','MS': 'Mato Grosso do Sul','MG': 'Minas Gerais','PA': 'Pará','PB': 'Paraíba','PR': 'Paraná','PE': 'Pernambuco','PI': 'Piauí','RJ': 'Rio de Janeiro','NR': 'Rio Grande do Norte','RS': 'Rio Grande do Sul','RO': 'Rondônia','RR': 'Roraima','SC': 'Santa Catarina','SP':'São Paulo','SE': 'Sergipe','TO': 'Tocntins','DF': 'Distrito Federal'};
-const selectEstado = document.querySelector('#estado');
-for(estado in estados) {
-  const newOption = document.createElement('option');
-  newOption.value = estado;
-  newOption.innerText = estados[estado];
-  selectEstado.appendChild(newOption)
+
+window.onload = function() {
+  const BUTTON_SUBMIT = document.querySelector('button');
+  const selectEstado = document.querySelector('#estado');
+  const inputNome = document.querySelector('#nome');
+  const estados = {'AC': 'Acre','AL': 'Alagoas','AP': 'Amapá','AM': 'Amazonas','BA': 'Bahia','CE': 'Ceará','ES': 'Espírito Santo','GO': 'Goiás','MA': 'Maranhão','MT': 'Mato Grosso','MS': 'Mato Grosso do Sul','MG': 'Minas Gerais','PA': 'Pará','PB': 'Paraíba','PR': 'Paraná','PE': 'Pernambuco','PI': 'Piauí','RJ': 'Rio de Janeiro','NR': 'Rio Grande do Norte','RS': 'Rio Grande do Sul','RO': 'Rondônia','RR': 'Roraima','SC': 'Santa Catarina','SP':'São Paulo','SE': 'Sergipe','TO': 'Tocntins','DF': 'Distrito Federal'};
+  for(estado in estados) {
+    const newOption = document.createElement('option');
+    newOption.value = estado;
+    newOption.innerText = estados[estado];
+    selectEstado.appendChild(newOption)
+  }
+
+  function buttonFunction(event) {
+    event.preventDefault();
+    inputNome
+  }
+
+  BUTTON_SUBMIT.addEventListener('click', buttonFunction);
 }
